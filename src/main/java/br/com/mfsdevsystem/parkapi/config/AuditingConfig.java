507a18a8cceb879080e56cf180353e64332e699e
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-12-22 17:42:29 -0400

    Create Audit

---
+++ src/main/java/br/com/mfsdevsystem/parkapi/config/AuditingConfig.java
@@ -0,0 +1,24 @@
+package br.com.mfsdevsystem.parkapi.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditingConfig implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null && authentication.isAuthenticated()) {
+			return Optional.of(authentication.getName());
+		}
+		return null;
+	}
+
+}
--- src/main/java/br/com/mfsdevsystem/parkapi/entity/Usuario.java
+++ src/main/java/br/com/mfsdevsystem/parkapi/entity/Usuario.java
@@ -4,9 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import br.com.mfsdevsystem.parkapi.enums.UserRole;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +22,7 @@ import jakarta.persistence.Table;
 
 @SuppressWarnings("serial")
 @Entity
+@EntityListeners( AuditingEntityListener.class)
 @Table(name = "usuarios")
 public class Usuario implements Serializable{
 
@@ -42,15 +49,19 @@ public class Usuario implements Serializable{
 	
 	//private Role role;
 	
+	@CreatedDate
 	@Column(name = "data_criacao")
 	private LocalDateTime dataCriacao;
 	
+	@LastModifiedDate
 	@Column(name = "data_modificacao")
 	private LocalDateTime dataModificacao;
 	
+	@CreatedBy
 	@Column(name = "criado_por")
 	private String criadoPor;
 	
+	@LastModifiedBy
 	@Column(name = "modificado_por")
 	private String modificadoPor;
 	
